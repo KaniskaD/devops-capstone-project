340f44a550d13d68e01f0d33eb3a1b2bdab82c4d
KaniskaD <kanihaniel@gmail.com>
2026-07-23 02:53:51 -0400

    implemented update account endpoint

--- service/routes.py
+++ service/routes.py
@@ -89,8 +89,29 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based on the body that is posted
+    """
+    app.logger.info("Request to update account with id: %s", account_id)
 
+    check_content_type("application/json")
+
+    account = Account.find(account_id)
+    if account is None:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id '{account_id}' was not found."
+        )
+
+    data = request.get_json()
+    account.deserialize(data)
+    account.id = account_id
+    account.update()
+
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
